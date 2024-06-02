83ba32b192b12ca2b2177dca3fd938bb44e8caac
Tevin Thomas <145091540+TevThom8@users.noreply.github.com>
2024-06-02 17:10:33 -0400

    Update mimopj1.py

--- mimopj1.py
+++ mimopj1.py
@@ -13,3 +13,5 @@ age_difference = age - bot_age
 print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
 color = input("What's your favorite color?")
 print(f"Oh, {color} is a beautiful color!")
+
+
